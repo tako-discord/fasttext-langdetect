20e9045d27fa5a44816a9d9a02cdedde3178c622
Pukimaa <pukima@pukima.site>
2022-12-25 15:30:45 +0100

    maybe that fixes tests

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -1,23 +1,20 @@
 import pytest
-from unittest import TestCase
 from ftlangdetect import detect
 
 pytest_plugins = ('pytest_asyncio',)
 
-class TestDetect(TestCase):
-
-    @pytest.mark.asyncio
-    async def test_detect_low_mem(self):
-        result = await detect("Bugün hava çok güzel", low_memory=True)
-        assert "lang" in result
-        assert "score" in result
-        assert isinstance(result["lang"], str)
-        assert isinstance(result["score"], float)
-        
-    @pytest.mark.asyncio
-    async def test_detect_high_mem(self):
-        result = await detect("Bugün hava çok güzel", low_memory=False)
-        assert "lang" in result
-        assert "score" in result
-        assert isinstance(result["lang"], str)
-        assert isinstance(result["score"], float)
+@pytest.mark.asyncio
+async def test_detect_low_mem(self):
+    result = await detect("Bugün hava çok güzel", low_memory=True)
+    assert "lang" in result
+    assert "score" in result
+    assert isinstance(result["lang"], str)
+    assert isinstance(result["score"], float)
+    
+@pytest.mark.asyncio
+async def test_detect_high_mem(self):
+    result = await detect("Bugün hava çok güzel", low_memory=False)
+    assert "lang" in result
+    assert "score" in result
+    assert isinstance(result["lang"], str)
+    assert isinstance(result["score"], float)
