506f2149f8909b15bf7963a291274150b407ba9f
Pukima <pukima@pukima.site>
2022-12-25 16:18:43 +0100

    v1.2.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='fasttext-langdetect',
-      version='1.1.3',
+      version='1.2.0',
       description='80x faster and 95% accurate language identification with Fasttext',
       keywords=['fasttext', 'langdetect', 'language detection',
                 'language identification'],
       long_description=open("README.md", "r", encoding='utf-8').read(),
       long_description_content_type="text/markdown",
       url='https://github.com/tako-discord/fasttext-langdetect.git',
-      download_url='https://github.com/tako-discord/fasttext-langdetect/archive/refs/tags/v1.1.3.tar.gz',
+      download_url='https://github.com/tako-discord/fasttext-langdetect/archive/refs/tags/v1.2.0.tar.gz',
       author='Pukima',
       author_email='pukima@pukima.site',
       install_requires=[
