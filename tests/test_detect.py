04a30ecc9228ca773cbd74056941b46f8052b20b
Pukimaa <pukima@pukima.site>
2022-12-25 15:32:44 +0100

    dumb mistake in tests

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -4,7 +4,7 @@ from ftlangdetect import detect
 pytest_plugins = ('pytest_asyncio',)
 
 @pytest.mark.asyncio
-async def test_detect_low_mem(self):
+async def test_detect_low_mem():
     result = await detect("Bugün hava çok güzel", low_memory=True)
     assert "lang" in result
     assert "score" in result
@@ -12,7 +12,7 @@ async def test_detect_low_mem(self):
     assert isinstance(result["score"], float)
     
 @pytest.mark.asyncio
-async def test_detect_high_mem(self):
+async def test_detect_high_mem():
     result = await detect("Bugün hava çok güzel", low_memory=False)
     assert "lang" in result
     assert "score" in result
